bd57d25fb2afb1637f766be50c10f64f9d455118
nan[ф] <56279981+nonobstant@users.noreply.github.com>
2023-05-22 15:54:51 +0200

    fix form

--- src/App.vue
+++ src/App.vue
@@ -16,24 +16,24 @@
         <div class="grid gap-6 mb-6 md:grid-cols-2">
             <div>
                 <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-                <input type="text" id="first_name" name="first_name" class="outline-none  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dodger-blue-500 focus:border-dodger-blue-500 block w-full p-2.5 dark:bg-dodger-blue-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dodger-blue-500 dark:focus:border-dodger-blue-500" placeholder="John" required>
+                <input v-model="form.firstname" type="text" id="first_name" name="first_name" class="outline-none  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dodger-blue-500 focus:border-dodger-blue-500 block w-full p-2.5 dark:bg-dodger-blue-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dodger-blue-500 dark:focus:border-dodger-blue-500" placeholder="" required>
             </div>
             <div>
                 <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
-                <input type="text" id="last_name" name="last_name" class="outline-none  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dodger-blue-500 focus:border-dodger-blue-500 block w-full p-2.5 dark:bg-dodger-blue-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dodger-blue-500 dark:focus:border-dodger-blue-500" placeholder="Doe" required>
+                <input v-model="form.lastname" type="text" id="last_name" name="last_name" class="outline-none  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dodger-blue-500 focus:border-dodger-blue-500 block w-full p-2.5 dark:bg-dodger-blue-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dodger-blue-500 dark:focus:border-dodger-blue-500" placeholder="" required>
             </div>
             <div>
                 <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Media</label>
-                <input type="text" id="media" name="media" class="outline-none  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dodger-blue-500 focus:border-dodger-blue-500 block w-full p-2.5 dark:bg-dodger-blue-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dodger-blue-500 dark:focus:border-dodger-blue-500" placeholder="Politico" required>
+                <input v-model="form.media" type="text" id="media" name="media" class="outline-none  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dodger-blue-500 focus:border-dodger-blue-500 block w-full p-2.5 dark:bg-dodger-blue-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dodger-blue-500 dark:focus:border-dodger-blue-500" placeholder="" required>
             </div>  
             <div>
                 <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
-                <input type="text" id="country" name="country" class="outline-none  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dodger-blue-500 focus:border-dodger-blue-500 block w-full p-2.5 dark:bg-dodger-blue-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dodger-blue-500 dark:focus:border-dodger-blue-500" placeholder="Belgium" required>
+                <input v-model="form.country" type="text" id="country" name="country" class="outline-none  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dodger-blue-500 focus:border-dodger-blue-500 block w-full p-2.5 dark:bg-dodger-blue-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dodger-blue-500 dark:focus:border-dodger-blue-500" placeholder="" required>
             </div>
         </div>
         <div class="mb-6">
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
-            <input type="email" id="email" name="email" class="outline-none  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dodger-blue-500 focus:border-dodger-blue-500 block w-full p-2.5 dark:bg-dodger-blue-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dodger-blue-500 dark:focus:border-dodger-blue-500" placeholder="john.doe@company.com" required>
+            <input v-model="form.email" type="email" id="email" name="email" class="outline-none  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dodger-blue-500 focus:border-dodger-blue-500 block w-full p-2.5 dark:bg-dodger-blue-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dodger-blue-500 dark:focus:border-dodger-blue-500" placeholder="" required>
         </div>
         <div class="flex items-start mb-6">
             <div class="flex items-center h-5">
